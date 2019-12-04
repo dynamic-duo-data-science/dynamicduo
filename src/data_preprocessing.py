c3fec9094a695cd55ae4781551b42d01258faa78
Chuan Sun <chuansun.sc@gmail.com>
2019-12-03 20:02:34 -0600

    preprocess datetime

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -3,24 +3,14 @@ import numpy as np
 import datetime
 
 raw = pd.read_csv("../data/raw/Border_Crossing_Entry_Data.csv")
+print "date read"
 raw['Date'] = pd.to_datetime(raw['Date'])
-
-coordinates_X, coordinates_Y = [], []
-# month_offset = []
-SMALLEST_YEAR = 2016
-for idx, row in raw.iterrows():
-    coordinate_str = row['Location'].split('POINT (')[1].split(' ')
-    co_x, co_y = coordinate_str[0], coordinate_str[1].split(')')[0]
-    coordinates_X.append(co_x)
-    coordinates_Y.append(co_y)
-    agg_month = (raw['Date'].dt.year - SMALLEST_YEAR) * 12 + raw['Date'].dt.month
-    # month_offset.append(agg_month)
-
-raw['location_x'] = coordinates_X
-raw['location_y'] = coordinates_Y
+print "date converted"
 raw['year'] = raw['Date'].dt.year
+print "year extracted"
 raw['month'] = raw['Date'].dt.month
+print "month extracted"
 
-raw.to_csv('../data/interim/interim.csv')
+raw.to_csv('../data/interim/date_transferred.csv')
 print "Done"
 
